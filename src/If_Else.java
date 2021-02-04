eac279b296258bcab3f693c0f1b3a4cd800f5ad3
Luis <luis_felipe_fc@hotmail.com>
2021-02-04 16:41:53 -0300

    If_Else

---
+++ src/If_Else.java
@@ -0,0 +1,21 @@
+
+public class If_Else {
+
+	public static void main(String[] args) {
+		int numero = 5;
+		
+		if(numero > 5) {
+			System.out.println("Sim, o número " + numero + " é maior que 5");
+		}else if(numero == 5) {
+			System.out.println("O número " + numero + " é igual a 5");
+		}else {
+			System.out.println("Não, o número " + numero + " não é maior que 5");
+		}
+		
+		if(numero % 2 == 0) {
+			System.out.println("Sim, o número " + numero + " é par");
+		}else {
+			System.out.println("Não, o número " + numero + " não é par");
+		}
+	}
+}
