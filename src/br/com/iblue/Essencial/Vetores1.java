8bc97caa6222529ab11a36d790d03bfec821ffca
Luis <luis_felipe_fc@hotmail.com>
2021-02-10 17:27:20 -0300

    Vetores

---
+++ src/br/com/iblue/Essencial/Vetores1.java
@@ -0,0 +1,35 @@
+package br.com.iblue.Essencial;
+
+//Arrays (Vetrores Uni-Dimensionais)
+public class Vetores1 {
+
+	
+	/* float valores[10]; 0..9
+	 * nt numeros[5]; 0..4 (n - 1) 5 - 1 = 4
+	 *
+	 * vetor[indice];
+	 * numeros[0] = 1;
+	 * numeros[1] = 3;
+	 * numeros[2] = 5;
+	 * numeros[3] = 7;
+	 * numeros[4] = 9;
+	 */
+	
+	public static void main(String[] args) {
+		
+		//Declarando um vetor
+		int num1[];
+		
+		//Declarando e especificando o tamanho do vetor.
+		int num2[] = new int[5];
+		
+		//Declarando, especificando o tamanho e inicializando o vetor.
+		int num3[] = {1, 3, 5, 7, 9};
+		
+		float num4[] = new float[5];
+		char caracteres[] = new char[20];
+		String nome[] = new String[4];
+		
+		String frutas[] = {"Manga", "Uva", "Banana"};
+	}
+}
---
+++ src/br/com/iblue/Essencial/Vetores2.java
@@ -0,0 +1,13 @@
+package br.com.iblue.Essencial;
+
+public class Vetores2 {
+	
+	public static void main(String[] args) {
+		
+		int numeros[] = new int[10];
+		
+		for(int i = 0; i < numeros.length; i++) {
+			
+		}
+	}
+}
